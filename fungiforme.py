668b44b05290673090f44d3bca0f4ba76c78bc08
Francesco Apruzzese <cescoap@gmail.com>
2022-06-07 18:31:44 +0200

    [ADD] Copyright headers

--- fungiforme.py
+++ fungiforme.py
@@ -1,5 +1,5 @@
-# Invite URL
-# https://discord.com/api/oauth2/authorize?client_id=964498743451856938&permissions=2147697728&scope=bot
+# Copyright 2022-TODAY Rapsodoo Italia S.r.L. (www.rapsodoo.com)
+# # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl)
 
 from os.path import exists
 import configparser
